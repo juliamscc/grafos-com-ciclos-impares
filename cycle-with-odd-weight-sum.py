9ca49cbde81dbc81a770772477e92cbf03d8c4e2
julia <juliamscc@gmail.com>
2023-09-16 10:04:44 -0300

    correção

--- cycle-with-odd-weight-sum.py
+++ cycle-with-odd-weight-sum.py
@@ -3,7 +3,7 @@ def createListOfEdges(graph):
 	listofedges = graph[1:]
 	edges = []
 	for i in listofedges:
-		e = list(map(int, i.split(' ')))
+		e = list(map(int, i.split( )))
 		edges.append(e)
 	return edges
 
@@ -12,14 +12,14 @@ def twoColorUtil(G, src, colorArr):
 	colorArr[src] = 1 # Colore o nó com a primeira cor
 	q = [src] # Cria uma fila com os nós a serem percorridos
 	
-	while len(q) > 0:
+	while q:
 		u = q.pop(0) # pega o primeiro nó da fila
 
-		for v in range(0, len(G[u])): # percorre os nós adjacentes a u
-			if colorArr[G[u][v]] == -1:
-				colorArr[G[u][v]] = 1 - colorArr[u] # colore com a cor direrente de u
-				q.append(G[u][v]) # adiciona o nó adjacente à fila
-			elif colorArr[G[u][v]] == colorArr[u]:		
+		for v in G[u]: # percorre os nós adjacentes a u
+			if colorArr[v] == -1:
+				colorArr[v] = 1 - colorArr[u] # colore com a cor direrente de u
+				q.append(v) # adiciona o nó adjacente à fila
+			elif colorArr[v] == colorArr[u]:		
 				return False
 	return True
 	
@@ -32,51 +32,36 @@ def twoColor(G, numNodes):
 
 	for i in range(numNodes):
 		if colorArr[i] == -1:
-			if twoColorUtil(G, i, colorArr) == False:
+			if not twoColorUtil(G, i, colorArr):
 				return False
 			return True
 
 # Função que verifica a existência de ciclos ímpares
 def isOddSum(edges, numNodes):
-	G = [[] for i in range(2*numNodes)] # Acrescenta um pseudo nó para cada aresta (no caso de todas serem pares)
-	
-	pseudo = numNodes + 1
-	pseudo_count = 0
+	G = [[] for i in range(numNodes)]
 
-	for i in range(1, len(edges)): # Percorre as arestas
-		u = edges[i][0] # Recebe o primeiro nó da aresta
-		v = edges[i][1] # Recebe o segundo nó da aresta
+	for edge in edges: # Percorre as arestas
+		u = edge[0] # Recebe o primeiro nó da aresta
+		v = edge[1] # Recebe o segundo nó da aresta
+		weight = edge[2] # Recebe o peso da aresta
 
-		if edges[i][2] % 2 == 1: # Se o peso for ímpar, apenas adiciona a aresta no grafo
-			G[u].append(v)
-			G[v].append(u)
-		else: 
-			# Adiciona um pseudo nó entre os nós da aresta
-			G[u].append(pseudo)
-			G[pseudo].append(u)
-			G[v].append(pseudo)
-			G[pseudo].append(v)
-			pseudo_count += 1
-			pseudo += 1
-		
-		if twoColor(G, numNodes + pseudo_count):
-			return False
-	return True
+		if weight % 2 == 1:
+			G[u-1].append(v-1)
+			G[v-1].append(u-1)
+
+	return not twoColor(G, numNodes)
 
 if __name__ == "__main__":
 # Leitura dos arquivos de texto
-	with open("testes\cows-test2.txt", "r") as file:
+	with open("testes\cows-test1.txt", "r") as file:
 		text = file.read()
 
-	# graph = text.split('\n')
+	graph = text.split('\n')
 
-	# numNodes = int(graph[0])
-	# edges = createListOfEdges(graph) 
-	# print(edges)
-	numNodes = 3
-	edges = [[1, 2, 1], [2, 3, 1], [1, 3, 1]]
+	numNodes = int(graph[0])
+	edges = createListOfEdges(graph) 
 
-	if isOddSum(edges, numNodes) == True:
+	if isOddSum(edges, numNodes):
 		print("Existem ciclos com peso ímpar no grafo.")
 	else:
 		print("Não existem ciclos com peso ímpar no grafo.")
