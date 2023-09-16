c571ca31c095a0717abaa3adfbf7dac9ca2b9e3f
julia <juliamscc@gmail.com>
2023-09-16 00:54:36 -0300

    Adicionando comentários no código

--- cycle-with-odd-weight-sum.py
+++ cycle-with-odd-weight-sum.py
@@ -1,72 +1,63 @@
-def twoColorUtil(G, src, N, colorArr):
-	
-	colorArr[src] = 1
-	
-	q = [src]
+# Função verifica se uma subparte da floresta pode ser colorida com duas cores
+def twoColorUtil(G, src, colorArr):
+	colorArr[src] = 1 # Colore o nó com a primeira cor
+	q = [src] # Cria uma fila com os nós a serem percorridos
 	
 	while len(q) > 0:
-		
-		u = q.pop(0)
+		u = q.pop(0) # pega o primeiro nó da fila
 
-		for v in range(0, len(G[u])):
-
+		for v in range(0, len(G[u])): # percorre os nós adjacentes a u
 			if colorArr[G[u][v]] == -1:
-			
-				colorArr[G[u][v]] = 1 - colorArr[u]
-				q.append(G[u][v])
-			
+				colorArr[G[u][v]] = 1 - colorArr[u] # colore com a cor direrente de u
+				q.append(G[u][v]) # adiciona o nó adjacente à fila
 			elif colorArr[G[u][v]] == colorArr[u]:		
 				return False
-		
 	return True
 	
-def twoColor(G, N):
-	
-	colorArr = [-1] * N
-	
-	for i in range(N):
+# Função que verifica se o grafo pode ser colorido com duas cores
+def twoColor(G, numNodes):
+	# Cria uma lista para guardar as cores marcadas no grafo
+	# -1 indica que o vétice não foi colorido
+	# 1 e 0 representam a primeira e a segunda cor marcada respectivamente
+	colorArr = [-1] * numNodes
+
+	for i in range(numNodes):
 		if colorArr[i] == -1:
-			if twoColorUtil(G, i, N, colorArr) == False:
+			if twoColorUtil(G, i, colorArr) == False:
 				return False
-			
 			return True
 
-def isOddSum(info, n, m):
+# Função que verifica a existência de ciclos ímpares
+def isOddSum(edges, numNodes, m):
+	G = [[] for i in range(2*numNodes)] # Acrescenta um pseudo nó para cada aresta (no caso de todas serem pares)
 	
-	G = [[] for i in range(2*n)]
-	
-	pseudo, pseudo_count = n+1, 0
-	for i in range(0, m):
-		
-		if info[i][2] % 2 == 1:
-			
-			u, v = info[i][0], info[i][1]
+	pseudo = numNodes + 1
+	pseudo_count = 0
+
+	for i in range(0, m): # Percorre as arestas
+		u = edges[i][0] # Recebe o primeiro nó da aresta
+		v = edges[i][1] # Recebe o segundo nó da aresta
+		if edges[i][2] % 2 == 1: # Se o peso for ímpar, apenas adiciona a aresta no grafo
 			G[u].append(v)
 			G[v].append(u)
-		
-		else:
-			u, v = info[i][0], info[i][1]
-
+		else: 
+			# Adiciona um pseudo nó entre os nós da aresta
 			G[u].append(pseudo)
 			G[pseudo].append(u)
 			G[v].append(pseudo)
 			G[pseudo].append(v)
-			
 			pseudo_count += 1
-
 			pseudo += 1
-	
-	return twoColor(G, n+pseudo_count)
+	return twoColor(G, numNodes + pseudo_count)
 
 if __name__ == "__main__":
-	
-	n, m = 4, 3
-	info = [[1, 2, 12],
+	numNodes, m = 4, 3
+	edges = [[1, 2, 12],
 			[2, 3, 1],
 			[4, 3, 1],
 			[4, 1, 20]]
-					
-	if isOddSum(info, n, m) == True:
-		print("No")
+
+	if isOddSum(edges, numNodes, m) == True:
+		print("Não existem ciclos com peso ímpar no grafo.")
 	else:
-		print("Yes")
+		print("Existem ciclos com peso ímpar no grafo.")
